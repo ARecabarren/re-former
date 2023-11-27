7219dd18d835c696571d94e2d0654f515d8e594d
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-11-27 12:41:06 -0300

    Set constraints

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true
-    validates :email, presence: true
-    validates :password, presence: true
+    validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 15 }
+    validates :email, presence: true, uniqueness: true, length: { maximum: 105 }
+    validates :password, presence: true, length: { minimum: 6, maximum: 25 }
 end

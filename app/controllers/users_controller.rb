3c6fa577b878138bbb184140f598243b7c320164
Arecabarren <arecabarren14@alumnos.utalca.cl>
2023-11-27 08:07:41 -0300

    Create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
-    # Build empty methods for #new and #create
+    # params = {"authenticity_token"=>"[FILTERED]", "user"=>{"username"=>"", "email"=>"", "password"=>"[FILTERED]"}
     def new
+        
     end
 
     def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to new_user_path
+        else
+            # Display the 422 status code, which means unprocessable entity
+            puts '422 unprocessable entity'
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
     end
 end
